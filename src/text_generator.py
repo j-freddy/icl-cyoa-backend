a2bc7916f732485c1bf6b18aae475552abbf5af2
Moss, Aidan <aidan.moss20@imperial.ac.uk>
2022-10-18 22:00:15 +0000

    Implement text generation including GPT-3 wrapper and data structure for the story

--- src/text_generator.py
+++ src/text_generator.py
@@ -1,3 +1,6 @@
+"""Module for the text generator.
+"""
+
 from typing import List
 from backend.src.tree import GamebookTree
 from backend.src.models.gpt3 import GPT3Model
@@ -11,13 +14,57 @@ class GamebookTextGenerator:
     def _option_prompt(self, num_options: int) -> str:
         return f"You have {num_options} options:"
 
-    def _generate_actions(self, paragraph: str, num_options=2) -> List[str]:
-        prompt = paragraph + " " + self._option_prompt(num_options)
+    def _generate_actions(self, full_text: str, num_options=2) -> List[str]:
+        prompt = full_text + " " + self._option_prompt(num_options)
         generated = self.model.complete(prompt)
         # Extract only the sentences.
-        actions = [line[line.find(next(filter(str.isalpha, line))):] for 
-                line in generated.splitlines() if line != ""]
+        actions = []
+        for line in generated.splitlines():
+            if line != "":
+                line = line[line.find(next(filter(str.isalpha, line))):]
+                if line[-1] != ".":
+                    line = line + "."
+                actions.append(line)
         return actions
 
-    def expand_graph(self, graph: GamebookTree, expand_at_node=0) -> None:
-        raise NotImplementedError
+    def _generate_paragraph(self, full_text: str) -> str:
+        prompt = full_text
+        return self.model.complete(prompt)
+
+    def _action_to_second_person(self, action: str) -> str:
+        return self.model.edit(text_to_edit=action,
+                              instruction="Rewrite this as 'You choose ...'")
+
+    def _paragraphs_to_prompt(self, paragraph_list: str):
+        return " ".join(paragraph_list)
+
+    def expand_graph_once(self, tree: GamebookTree, expand_at_node=0) -> None:
+        """Expand the graph at the specified node: Generate the paragraph if it
+        is missing, and generate the action options for the paragraph otherwise.
+        """
+
+        paragraph_list = tree.get_paragraph_list(expand_at_node)
+        end_action = tree.get_action(expand_at_node)
+        end_paragraph = tree.get_paragraph(expand_at_node)
+
+        previous_text = self._paragraphs_to_prompt(paragraph_list)
+
+        if end_paragraph is None:
+            if end_action is None:
+                # first node - generate starter paragraph
+                raise NotImplementedError
+
+            edited_action = self._action_to_second_person(end_action) + " "
+
+            # then we need to generate paragraph
+            generated_paragraph = self._generate_paragraph(previous_text + " "
+                + edited_action)
+            tree.edit_node(expand_at_node,
+                paragraph =  edited_action + generated_paragraph)
+            return
+
+        # else we need to generate new actions given the current text and
+        # create new nodes for these actions
+        generated_actions = self._generate_actions(previous_text)
+        for generated_action in generated_actions:
+            tree.make_node(parent_id=expand_at_node, action=generated_action)
--- src/tree.py
+++ src/tree.py
@@ -1,45 +1,96 @@
+""" Module for the tree representation of the gamebook.
+"""
 from typing import List, Optional
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 from dataclasses_json import dataclass_json, LetterCase
+
 
 @dataclass_json(letter_case=LetterCase.CAMEL)
 @dataclass
 class GamebookNodeData:
+    """Dataclass which can be converted to and from json-style camelCase using
+    to_dict and from_dict"""
     node_id: int
 
     action: Optional[str]
-    paragraph: str
+    paragraph: Optional[str]
 
     parent_id: Optional[int]
-    children_ids: List[int]
+    children_ids: List[int] = field(default_factory=list)
+
 
 class GamebookTree:
     """Class for representing the tree for a gamebook"""
 
     def __init__(self, node_data_list: List[GamebookNodeData]):
-        self.node_lookup = {node_data.node_id: node_data for node_data in node_data_list}
+        self.node_lookup = {node_data.node_id: node_data
+                            for node_data in node_data_list}
+
+        self.next_node_id = max(
+            node_data.node_id for node_data in node_data_list) + 1
 
     @staticmethod
     def from_nodes_dict_list(node_data_dict_list):
-        node_data_list = [GamebookNodeData.from_dict(node_data) for node_data in node_data_dict_list]
+        """Deserializes from json-style dictionary to GamebookTree"""
+        node_data_list = [GamebookNodeData.from_dict(node_data)
+                          for node_data in node_data_dict_list]
 
         return GamebookTree(node_data_list)
 
     def to_nodes_dict_list(self):
+        """Serializes class data into a json-style dictionary with list of node
+        data"""
         return [node_data.to_dict() for node_data in self.node_lookup.values()]
 
-    def get_text_up_to_node(self, end_node_id):
-        rev_text = []
-        node_id = self.node_lookup[end_node_id].parent_id
+    def _get_node(self, node_id):
+        return self.node_lookup[node_id]
+
+    def make_node(self, parent_id, action=None, paragraph=None):
+        """Allow editing action and paragraph"""
+        parent = self.node_lookup[parent_id]
+
+        new_node = GamebookNodeData(
+            node_id=self.next_node_id, action=action, paragraph=paragraph, parent_id=parent_id)
+
+        self.node_lookup[self.next_node_id] = new_node
+
+        parent.children_ids.append(self.next_node_id)
+        self.next_node_id += 1
+
+    def edit_node(self, node_id, action=None, paragraph=None):
+        """Allow editing action and paragraph"""
+        node = self.node_lookup[node_id]
+
+        if action is not None:
+            node.action = action
+
+        if paragraph is not None:
+            node.paragraph = paragraph
+
+    def get_action(self, node_id):
+        """Get the action at node with the node_id.
+        """
+        return self.node_lookup[node_id].action
+
+    def get_paragraph(self, node_id):
+        """Get the action at node with the node_id.
+        """
+        return self.node_lookup[node_id].paragraph
+
+    def get_paragraph_list(self, end_node_id):
+        """Generates a list of paragraph from the root node to end node
+        inclusively."""
+        rev_paragraphs = []
+
+        # here we need to build the text list in reverse, since we need to
+        # traverse the tree using the parent_id field
+        node_id = end_node_id
 
         while node_id is not None:
             node = self.node_lookup[node_id]
-
-            rev_text.append(node.paragraph)
-            if node.action is not None:
-                rev_text.append(node.action)
-
+            if node.paragraph is not None:
+                rev_paragraphs.append(node.paragraph)
             node_id = node.parent_id
 
-        return "".join(reversed(rev_text))
+        return list(reversed(rev_paragraphs))
